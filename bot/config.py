465f3020bfe4eac473d90a2eef2227c5fa3e150f
ken862734801 <kenshawmichaud@gmail.com>
2025-05-01 12:52:08 -0400

    fix config leak

--- bot/config.py
+++ bot/config.py
@@ -8,8 +8,8 @@ class Config:
     BOT_ID = os.getenv("TWITCH_BOT_ID")
     CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
     CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
-    SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndveW9odWV3ZmtiemFyeGFhenRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNzUwMzMsImV4cCI6MjA2MDY1MTAzM30.iclPSTZDqFw8BLhzYTiFervTrovk0USFtzt6ff5Oo8M"
-    SUPABASE_URL="https://woyohuewfkbzarxaaztq.supabase.co"
+    SUPABASE_KEY= os.getenv("SUPABASE_KEY")
+    SUPABASE_URL= os.getenv("SUPABASE_URL")
     TWITCH_ACCESS_TOKEN = os.getenv("TWITCH_ACCESS_TOKEN")
     TWITCH_REFRESH_TOKEN = os.getenv("TWITCH_REFRESH_TOKEN")    
     
